0640ea0dd5bc2f632650a41dbad262370783eaa4
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 19:08:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,18 +5,19 @@ def my_hash
 end
 
   # set a variable called `the_manifest`, equal to a hash
+  # fill that hash with key/value pairs that describe the following information:
+  # We have 5 "whale bone corsets", 2 "porcelain vases" and 3 "oil painting"
 def shipping_manifest
 the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
-  # fill that hash with key/value pairs that describe the following information:
-  # We have 5 "whale bone corsets", 2 "porcelain vases" and 3 "oil painting"_manifest = {
+
 
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 
   # add 2 muskets to the shipping_manifest hash below
-
+  the_manifest["muskets"]= 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
