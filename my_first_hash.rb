b34a71b3a5d2b93f7f219b3248e0b695bd6f59da
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 18:57:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,16 +1,16 @@
 
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  
+
 
 
 
 
 
   # set a variable called `the_manifest`, equal to a hash
-  
+
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil painting_manifest = {
-                  
+
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
@@ -22,5 +22,3 @@
 
 
   # return the shipping_manifest hash below
-
-
