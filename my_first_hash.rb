4af2cae4a4bf2a9a50df1ff15f31501e8ae4ee9b
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 19:13:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,13 +14,13 @@ end
 
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets"] = 2
+  the_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"] = 4
+  the_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
 shipping_manifest
