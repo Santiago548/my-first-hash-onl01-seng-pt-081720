8d2df19bb49b1eb5a99bc87fde0ed2b54b5833f3
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 19:01:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,8 @@
 
 # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"dog1" => "Dixie", "dog2" => "Stella"}
-
+def my_hash
+  my_hash = {"dog1" => "Dixie", "dog2" => "Stella"}
+end
 
 
 
